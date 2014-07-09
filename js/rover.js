a2de57ece3ab78fb76e199f6de14cd0cddf5c380
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-07-09 11:14:40 +0200

    added a fnction

--- js/rover.js
+++ js/rover.js
@@ -4,12 +4,23 @@
   moveBackward: function () {
     this.advance('backward');
   },
-  advance: function (direction) {
+  dvance: function (direction) {
     direction = (direction === 'forward') ? 1 : -1;
-    var formerPosition = [
-      this.position[0],
-      this.position[1]
-    ];
+    var X = this.position[0], Y = this.position[1];
+
+    var increment = this.increments[this.orientation];
+    X = (X + increment[0] * direction) % Grid.sizeX;
+    Y = (Y + increment[1] * direction) % Grid.sizeY;
+    if (X < 0) {
+      X += Grid.sizeX;
+    }
+    if (Y < 0) {
+      Y += Grid.sizeY;
+    }
+    if (!Grid.thereIsObstacle(X, Y)) {
+      this.position = [X, Y];
+    }
+  },
     var increment = this.increments[this.orientation];
     this.position = [
       (this.position[0] + increment[0] * direction) % Grid.sizeX,
